d3fe44262b5b80f8a2fd9eb79e975d955c9ca290
spearscjs <spearscjs@gmail.com>
2022-04-11 23:11:55 -0700

    added frontend UI changes for unread messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -18,6 +18,28 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  unreadPreviewText: {
+    color: 'black',
+    letterSpacing: -0.17,
+  },
+  unreadBubble: {
+    height: 24,
+    borderRadius: 12,
+    padding: 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3A8DFF',
+    marginRight: 25,
+    alignSelf: 'center'
+  },
+  text: {
+    fontSize: 12,
+    letterSpacing: -0.2,
+    padding: 8,
+    color: '#FFFFFF',
+    
+  },
 }));
 
 const ChatContent = ({ conversation }) => {
@@ -25,6 +47,9 @@ const ChatContent = ({ conversation }) => {
 
   const { otherUser } = conversation;
   const latestMessageText = conversation.id && conversation.latestMessageText;
+  const latestMessageIsRead = conversation.messages.length > 0 && 
+                                conversation.messages[conversation.messages.length-1].isRead || 
+                                conversation.messages[conversation.messages.length-1].senderId !== otherUser.id;
 
   return (
     <Box className={classes.root}>
@@ -32,11 +57,11 @@ const ChatContent = ({ conversation }) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={latestMessageIsRead ? classes.previewText : classes.unreadPreviewText}>
           {latestMessageText}
         </Typography>
       </Box>
-      <Box>
+      <Box className = {classes.unreadBubble}>
         <Typography className={classes.text}>{conversation.messages.filter(m => m.senderId === otherUser.id && m.isRead === false).length}</Typography>
       </Box>
       
