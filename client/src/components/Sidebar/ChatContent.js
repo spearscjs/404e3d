41fc13cddd89793614e3abb79808127a3eba4019
spearscjs <spearscjs@gmail.com>
2022-04-14 10:54:44 -0700

    rid of unused import

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { normalizeUnits } from "moment";
 
 const useStyles = makeStyles((theme) => ({
   root: {
